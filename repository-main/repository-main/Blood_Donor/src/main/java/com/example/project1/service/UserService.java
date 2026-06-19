b8062c62a0c86603b918a7d8c9e8331fdad59730
musinichayadevi-lab <musinichayadevi@gmail.com>
2026-06-19 10:33:09 +0530

    Add files via upload

---
+++ repository-main/repository-main/Blood_Donor/src/main/java/com/example/project1/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.project1.service;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.project1.Entity.UserEntity;
+import com.example.project1.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<UserEntity> loginUser(
+            String email,
+            String password,
+            String role) {
+
+        return userRepository.findByEmailAndPasswordAndRole(
+                email,
+                password,
+                role);
+        
+    }
+
+	public Optional<UserEntity> findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
